a28d6a5f198522a05b1ac6bcde404fdc6ef7898a
pretlowjc <justin.pretlow13@gmail.com>
2023-12-03 15:15:29 -0500

    Deleting pre-made stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PerkTierComponent } from './perk-tier/perk-tier.component';
+import { ChecklistComponent } from './checklist/checklist.component';
+import { LinksComponent } from './links/links.component';
+import { DpsComponent } from './dps/dps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PerkTierComponent,
+    ChecklistComponent,
+    LinksComponent,
+    DpsComponent
   ],
   imports: [
     BrowserModule
